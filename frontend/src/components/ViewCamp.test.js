a486c93de0c61a99dcaa38fa27138e2912d9c6c7
William Dalian <williamdalian1@gmail.com>
2024-11-15 10:17:31 -0500

    Include tests in the codebase
    Fix: Modify git ignore to include tests.
    Include ViewCamp.test.js

---
+++ frontend/src/components/ViewCamp.test.js
@@ -0,0 +1,7 @@
+import {render, screen} from "@testing-library/react"
+import CampView from "./ViewCamp"
+
+test('on inital render, the page displays loading camp details',()=>{
+    render(<CampView />);
+    screen.debug();
+})
