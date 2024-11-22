c5aa2c88546435c6e8f6d907d47c5087370f106d
EvanMeyer36 <emeyer5@emich.edu>
2024-11-22 12:32:54 -0500

    Update campgrounds.js

--- backend/routes/campgrounds.js
+++ backend/routes/campgrounds.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
